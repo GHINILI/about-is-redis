bfc6246b258dfd8b0ba71df020dcf1c919e46781
GHINILI <152369167+GHINILI@users.noreply.github.com>
2023-12-08 12:02:25 +0800

    Create CheckUtils.java

---
+++ CheckUtils.java
@@ -0,0 +1,27 @@
+package com.lwj.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+  *查询工具类
+  */
+@Component
+@Slf4j
+public class CheckUtils
+{
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    public boolean checkWithBloomFilter(String checkItem,String key)
+    {
+        int hashValue = Math.abs(key.hashCode());
+        long index = (long) (hashValue % Math.pow(2, 32));
+        boolean existOK = redisTemplate.opsForValue().getBit(checkItem, index);
+        log.info("----->key:"+key+"\t对应坑位index:"+index+"\t是否存在:"+existOK);
+        return existOK;
+    }
+}
